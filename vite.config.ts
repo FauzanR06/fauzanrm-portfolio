8ef80b1f5b6cad118bd445fd377a933587beca58
FauzanR06 <fauzanm157@gmail.com>
2026-06-16 16:08:40 +0700

    fix: update base path to relative URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/fauzanrm-portfolio/',
+  base: './fauzanrm-portfolio/',
 })
