e2232890d0c25e130fc23e1f6070b5c08f4c64d2
FauzanR06 <fauzanm157@gmail.com>
2026-06-16 15:57:58 +0700

    chore: update base path in vite config for repository deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'fauzanr06.github.io/',
+  base: '/fauzanrm-portfolio/',
 })
